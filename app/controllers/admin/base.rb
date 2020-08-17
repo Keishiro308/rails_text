91f7a659fffa0a1720e99e61dd11e6a9ee70b50a
Keishiro Nishiyama <kn11308@gmail.com>
2020-08-17 12:50:44 +0900

    12章終了

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,4 +1,40 @@
 class Admin::Base < ApplicationController
+  before_action :authorize
+  before_action :check_account
+  before_action :check_timeout
+
+
+  private def authorize
+    unless current_administrator
+      flash.alert = "管理者としてログインしてください。"
+      redirect_to :admin_login
+    end
+  end
+
+  private def check_account
+    if current_administrator && current_administrator.suspended?
+      flash.alert = "アカウントが停止されています。"
+      redirect_to :admin_root
+    end
+  end
+
+  TIMEOUT = 60.minutes
+  private def check_timeout
+    if session[:last_access_time]
+      if session[:last_access_time] < TIMEOUT.ago
+        session.delete(:last_access_time)
+        flash.alert = "セッションがタイムアウトしました。"
+        redirect_to :admin_login
+      else
+        session[:last_access_time] = Time.current
+      end
+    end
+  end
+  
+  
+  
+
+
   private def current_administrator
     if session[:administrator_id]
       @current_administrator ||=
