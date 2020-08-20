16dda44cefaec84f2edbc5b91a4f69af0116d8f1
Keishiro Nishiyama <kn11308@gmail.com>
2020-08-20 23:49:13 +0900

    15章終了

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -21,7 +21,7 @@ class Staff::Base < ApplicationController
   TIMEOUT = 60.minutes
 
   private def check_timeout
-    if current_staff_member
+    if session[:last_access_time]
       if session[:last_access_time] >= TIMEOUT.ago
         session[:last_access_time] = Time.current
       else
