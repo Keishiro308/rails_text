50f39b6c6669a5ac219ced5111a0543ded8e1b7e
Keishiro Nishiyama <kn11308@gmail.com>
2020-09-03 17:31:26 +0900

    7章終了

--- app/javascript/packs/staff.js
+++ app/javascript/packs/staff.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "../staff/customer_form.js";
+import "../staff/entries_form.js";
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
